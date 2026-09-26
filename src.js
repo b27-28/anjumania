66aea850805623336c72123f9fef1573e3b12d52
b27-28 <b27-28@mail.ru>
2026-09-26 12:05:02 +0200

    Update src.js

--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-$u334410653=[
-  { date: '26.09.2026', bonus: 25 },
-  { date: '26.09.2026', bonus: 30 }
+let database=[
+  { user:31140, date: '26.09.2026', bonus: 25 },
+  { user:27650, date: '26.09.2026', bonus: 30 }
 ];
